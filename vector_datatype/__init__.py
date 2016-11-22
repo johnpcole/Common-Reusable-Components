6c09b9cc9b94c87834250844a0d4d815b96d43e8
Dr J P Cole <johnpcole@users.noreply.github.com>
2016-11-22 00:37:34 +0000

    Fixed create functions

--- vector_datatype/__init__.py
+++ vector_datatype/__init__.py
@@ -1,4 +1,8 @@
-# Version 5.1
+# Version 5.2
+
+# - createblank, createorigin fixed
+
+# ====================================================================================# Version 5.1
 
 # - getinverted, getswapped, getint, getfloat, getrotated, getscaled fixed
 
