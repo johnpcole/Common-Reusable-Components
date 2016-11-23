09499e37b3a4bc15768c802f5596f0d7fe5bc479
Dr J P Cole <johnpcole@users.noreply.github.com>
2016-11-23 13:31:37 +0000

    Getstate added and setareastate improved

--- appinput_framework/__init__.py
+++ appinput_framework/__init__.py
@@ -1,1 +1,8 @@
+Version 5.1
+
+Renamed getcurrentmousebutton to calculatecurrentmousebutton
+setareastate updated to include valid state names
+getareastate added
+
+# =======================================
 # Version 5.0
