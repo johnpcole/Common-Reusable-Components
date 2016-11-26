0b1fc7de1eec04d37f8f6beb0fbab9e0693d0ec6
Dr J P Cole <johnpcole@users.noreply.github.com>
2016-11-26 15:10:22 +0000

    Fixed image-load to be OS agnostic

--- appdisplay_framework/__init__.py
+++ appdisplay_framework/__init__.py
@@ -1,3 +1,8 @@
+# Version 5.2
+
+# - fixed add to be OS agnostic (directory structure slashes)
+
+# ====================================================================================
 # Version 5.1
 
 # - merged drawbox and drawboxoutline into drawrectangle
